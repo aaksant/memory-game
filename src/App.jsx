587535705a3efa453dd3c0c0d353a712b8678d56
aaksant <aaksangit@gmail.com>
2025-01-31 10:10:55 +0700

    feat: display toast if there are errors in fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Toaster } from 'react-hot-toast';
 import Game from './components/game/Game';
 
 export default function App() {
   return (
     <div className="flex flex-col items-center p-4">
       <Game />
+      <Toaster />
     </div>
   );
 }
