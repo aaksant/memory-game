1f12fa3ca1ea893b3bedd6f97b88706c581d1971
aaksant <aaksangit@gmail.com>
2025-02-01 14:14:12 +0700

    feat: add `gameState` state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 import Game from './components/game/Game';
+import Home from './components/home/Home';
 
 export default function App() {
+  const [gameState, setGameState] = useState({
+    isPlaying: false,
+    level: null
+  });
+
+  const handleStartGame = level => {
+    setGameState({
+      isPlaying: true,
+      level: level
+    });
+  };
+
   return (
-    <div className="flex flex-col items-center p-4">
-      <Game />
+    <div className="flex min-h-screen w-full flex-col items-center justify-center p-4">
+      {!gameState.isPlaying ? (
+        <Home handleStartGame={handleStartGame} />
+      ) : (
+        <Game level={gameState.level} />
+      )}
       <Toaster />
     </div>
   );
