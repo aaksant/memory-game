c610d3c5649e98c67063c61f499b318fad045bb6
aaksant <aaksangit@gmail.com>
2025-02-01 14:59:19 +0700

    feat: minor changes to cards gap and image

--- src/components/game/Card.jsx
+++ src/components/game/Card.jsx
@@ -1,7 +1,11 @@
 export default function Card({ image_url, name }) {
   return (
     <article className="w-64 transform cursor-pointer overflow-hidden rounded-lg bg-white shadow-lg transition-transform hover:shadow-xl md:w-56">
-      <img src={image_url} alt={name} className="h-56 w-full object-top" />
+      <img
+        src={image_url}
+        alt={name}
+        className="h-56 w-full rounded-t-lg border-none object-top"
+      />
       <div className="p-4 text-center">
         <p className="text-md font-semibold text-gray-800">{name}</p>
       </div>
--- src/components/game/Cards.jsx
+++ src/components/game/Cards.jsx
@@ -2,8 +2,8 @@ import Card from './Card';
 
 export default function Cards({ characters }) {
   return (
-    <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-      {characters.map((character) => (
+    <div className="grid grid-cols-1 justify-items-center gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {characters.map(character => (
         <Card {...character} key={crypto.randomUUID()} />
       ))}
     </div>
