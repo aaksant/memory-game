979f692f44bb14c86194c69c1ccb7f30e9b7fbe1
aaksant <aaksangit@gmail.com>
2025-02-02 14:27:18 +0700

    feat: add `rounds` property

--- src/levels.js
+++ src/levels.js
@@ -1,7 +1,7 @@
 const levels = [
-  { text: 'Easy', numberOfCards: 3, rounds: 5 },
-  { text: 'Medium', numberOfCards: 4, rounds: 7 },
-  { text: 'Hard', numberOfCards: 5, rounds: 10 }
+  { text: 'Easy', numberOfCards: 3, rounds: 10 },
+  { text: 'Medium', numberOfCards: 4, rounds: 15 },
+  { text: 'Hard', numberOfCards: 5, rounds: 20 }
 ];
 
 export default levels;
