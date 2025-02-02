cfc9befd6d2d4bc480c691696056d2e5921de302
aaksant <aaksangit@gmail.com>
2025-02-02 18:21:25 +0700

    chore: minor changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,7 +3,8 @@ import LevelButtons from './LevelButtons';
 export default function Home({ handleStartGame }) {
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="mb-8 text-3xl font-bold">Memory Game</h1>
+      <h1 className="mb-4 text-3xl font-bold">Memory Game</h1>
+      <h2 className="text-md mb-10">Select your level</h2>
       <LevelButtons handleStartGame={handleStartGame} />
     </div>
   );
