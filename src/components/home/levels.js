1b42be8b73cddf36e3aaa1ca5f85917f277d90c7
aaksant <aaksangit@gmail.com>
2025-02-02 13:04:08 +0700

    refactor: move `levels.js` to `src/`

--- src/components/home/levels.js
+++
@@ -1,7 +0,0 @@
-const levels = [
-  { text: 'Easy', numberOfCards: 3},
-  { text: 'Medium', numberOfCards: 4},
-  { text: 'Hard', numberOfCards: 5}
-]
-
-export default levels;
---
+++ src/levels.js
@@ -0,0 +1,7 @@
+const levels = [
+  { text: 'Easy', numberOfCards: 3, rounds: 5 },
+  { text: 'Medium', numberOfCards: 4, rounds: 7 },
+  { text: 'Hard', numberOfCards: 5, rounds: 10 }
+];
+
+export default levels;
