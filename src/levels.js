dc4f042609fc8b53decb013bc1a7522c833e852d
aaksant <aaksangit@gmail.com>
2025-02-06 11:25:37 +0700

    chore: change level name

--- src/levels.js
+++ src/levels.js
@@ -1,7 +1,7 @@
 const levels = [
   { name: 'Otaku', numberOfCards: 3, rounds: 5 },
   { name: 'Weeb', numberOfCards: 4, rounds: 10 },
-  { name: 'Discord Mod', numberOfCards: 5, rounds: 20 }
+  { name: 'Reddit Mod', numberOfCards: 5, rounds: 20 }
 ];
 
 export default levels;
